1b7b1705abf1e541a49b95358197aaaf38e64375
shivam vishwakarma <vishwakarmashivam2003@gmail.com>
2023-08-08 22:22:05 +0530

    inital commit

---
+++ pages/agentList.js
@@ -0,0 +1,73 @@
+import Layout from "@/components/Layout";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+export default function AgentList() {
+  const [orders, setOrders] = useState([]);
+  useEffect(() => {
+    axios.get("/api/orders").then((response) => {
+      setOrders(response.data);
+    });
+  }, []);
+  return (
+    <Layout>
+      <div className="flex flex-row justify-between">
+        <h1>Agent List</h1>
+        <button className="px-4 mb-4 py-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-indigo-300">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M19 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zM4 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 0110.374 21c-2.331 0-4.512-.645-6.374-1.766z"
+            />
+          </svg>
+        </button>
+      </div>
+      <table className="basic">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Phone</th>
+            <th>Password</th>
+            <th>STATUS</th>
+            <th>ACTION</th>
+          </tr>
+        </thead>
+        <tbody>
+          {orders.length > 0 &&
+            orders.map((order) => (
+              <tr key={order._id}>
+                <td>{new Date(order.createdAt).toLocaleString()}</td>
+                <td className={order.paid ? "text-green-600" : "text-red-600"}>
+                  {order.paid ? "YES" : "NO"}
+                </td>
+                <td>
+                  {order.name} {order.email}
+                  <br />
+                  {order.city} {order.postalCode} {order.country}
+                  <br />
+                  {order.streetAddress}
+                </td>
+                <td>
+                  {order.line_items.map((l) => (
+                    <>
+                      {l.price_data?.product_data.name} x{l.quantity}
+                      <br />
+                    </>
+                  ))}
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </Layout>
+  );
+}
---
+++ pages/contact.js
@@ -0,0 +1,93 @@
+import React from "react";
+import Layout from "@/components/Layout";
+import { useState } from "react";
+
+export default function Contact() {
+
+  const [formData, setFormData] = useState({
+    senderAddress: '',
+    senderEmail: '',
+    senderPhone: '',
+    recipientEmail: '',
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle form submission here, e.g., sending emails
+    console.log(formData); // Replace with your actual submission logic
+  };
+
+
+  return (
+   
+    <Layout>
+      <h1>Contact info</h1>
+      <div className="flex justify-center ">
+      <div className="max-w-lg p-6 bg-transparent rounded-lg shadow-md w-full">
+        <div className="mb-4">
+          <p className="text-gray-600">Address:</p>
+          <textarea
+            name="senderAddress"
+            className="mt-1 p-2 block w-full border rounded-md focus:ring focus:ring-indigo-300 focus:border-indigo-300"
+            placeholder="Enter address"
+            value={formData.senderAddress}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="mb-4">
+          <p className="text-gray-600">Email:</p>
+          <input
+            type="email"
+            name="senderEmail"
+            className="mt-1 p-2 block w-full border rounded-md focus:ring focus:ring-indigo-300 focus:border-indigo-300"
+            placeholder="Enter email address"
+            value={formData.senderEmail}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="mb-4">
+          <p className="text-gray-600">Phone:</p>
+          <input
+            type="tel"
+            name="senderPhone"
+            className="mt-1 p-2 block w-full border rounded-md focus:ring focus:ring-indigo-300 focus:border-indigo-300"
+            placeholder="Enter phone number"
+            value={formData.senderPhone}
+            onChange={handleInputChange}
+          />
+        </div>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label className="block text-sm font-medium text-gray-700">Send To</label>
+            <input
+              type="email"
+              name="recipientEmail"
+              className="mt-1 p-2 block w-full border rounded-md focus:ring focus:ring-indigo-300 focus:border-indigo-300"
+              placeholder="Enter recipient's email address"
+              value={formData.recipientEmail}
+              onChange={handleInputChange}
+            />
+          </div>
+          <button
+            type="submit"
+            className="px-4 py-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-indigo-300"
+          >
+            Send Message
+          </button>
+        </form>
+      </div>
+    </div>
+
+    
+    </Layout>
+ 
+  );
+}
